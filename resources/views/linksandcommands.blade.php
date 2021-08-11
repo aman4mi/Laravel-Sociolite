63d2d8ddac03ebe2213ef7cd356dff9a4e1f35c6
aman4mi <aman4.mi@gmail.com>
2021-08-12 00:51:25 +0600

    first commit

---
+++ resources/views/linksandcommands.blade.php
@@ -0,0 +1,11 @@
+composer create-project --prefer-dist laravel/laravel myfirstproject   //creating project   
+composer require laravel/ui --dev
+php artisan ui bootstrap --auth
+npm install  // npm install laravel-mix@latest --save-dev
+npm clean-install
+npm run dev
+composer require laravel/socialite                       
+php artisan config:cache  //after change in .env file
+
+//171devteam@gmail.com  //171devteam_171///mail info
+https://www.youtube.com/watch?v=jIckLu1cKew
